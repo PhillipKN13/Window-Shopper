6c33e96fea105783da712007b522f93e716a4e56
Phillip Nguyen <phillip77478@gmail.com>
2018-01-08 17:00:53 -0600

    description of changes

---
+++ window-shopper/Model/Wage.swift
@@ -0,0 +1,9 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Phillip Nguyen on 1/7/18.
+//  Copyright © 2018 Phillip Nguyen. All rights reserved.
+//
+
+import Foundation
---
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Phillip Nguyen on 1/7/18.
+//  Copyright © 2018 Phillip Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
